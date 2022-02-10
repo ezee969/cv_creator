231ccbd5c4430dce3af601f06f30d012c151e312
Ezequiel <ezee.-96@gmail.com>
2022-02-09 23:04:46 -0300

    fc

---
+++ src/components/pages/footer.js
@@ -0,0 +1,10 @@
+import icon from "../../assets/gh-icon.pgn"
+
+export default function Footer() {
+    return(
+        <div className="footer-cont">
+            <p id="footer-text">Copyright © 2022 ezee969</p>
+            <img src={icon} alt="icon"></img>
+        </div>
+    )
+}
---
+++ src/components/pages/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+        <div className="header-cont">
+            <h1 id = "h-text">cv_creator</h1>
+        </div>
+    )
+}
